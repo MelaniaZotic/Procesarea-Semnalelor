b0bb8f22ae5da90b6f0fccec4ed1892a860b805c
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2023-10-26 18:10:40 +0300

    Exercitii laborator 2

--- Lab2/ex2.py
+++ Lab2/ex2.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io.wavfile
-import scipy.signal
 
 # Definirea parametrilor:
 amplitudine = 1
@@ -11,35 +9,41 @@ t = np.arange(0, 1, 0.01)
 faze = [np.pi / 2, np.pi / 3, np.pi / 4, np.pi / 6]
 semnale = [amplitudine * np.sin(2 * np.pi * frecventa * t + faza) for faza in faze]
 
+# Afisarea semnalelor
 plt.figure(figsize=(10, 6))
-for semnal, faza in zip(semnale, faze):
-    plt.plot(t, semnal, label=f'Faza = {faza} rad')
+for signal, faza in zip(semnale, faze):
+    plt.plot(t, signal, label=f'Faza = {faza}')
+plt.legend()
 plt.title('Semnale sinusoidale cu faze diferite')
-plt.xlabel('Timp')
+plt.xlabel('Timp (s)')
 plt.ylabel('Amplitudine')
 plt.grid(True)
 plt.show()
 
+
+# am creat o functie pentru a calcula gamma dupa formula din enunt
 
 def calc_gamma(s, z, SNR):
     semnal_patrat = np.linalg.norm(s) ** 2
     zgomot_patrat = np.linalg.norm(z) ** 2
     g = np.sqrt(semnal_patrat / (SNR * zgomot_patrat))
-
     return g
 
+# x este primul semnal
+x = semnale[0]
+# zgomotul
+z = np.random.randn(len(t))
+SNRs = [0.1, 1, 10, 100]
 
-zgomot_Gaussian = np.random.normal(1, len(semnale))
-snrs = [0.1, 1, 10, 100]
-
-for snr in snrs:
-    gamma = calc_gamma(semnale[0], zgomot_Gaussian, snr)
-    zgomot_semnal = semnale[0] + gamma * zgomot_Gaussian
+plt.figure(figsize=(12, 8))
+for snr in SNRs:
+    gamma = calc_gamma(x, z, snr)
+    zgomot_semnal = x + gamma * z
     plt.plot(t, zgomot_semnal, label=f'SNR = {snr}')
+
 plt.legend()
-plt.title('Semnal sinusoidal cu zgomot Gaussian pentru diferite SNRs')
-plt.xlabel('Timp')
+plt.title('Semnalul cu faza pi/2 avand zgomot adaugat pentru diferite valori de SNR')
+plt.xlabel('Timp (s)')
 plt.ylabel('Amplitudine')
 plt.grid(True)
 plt.show()
-
---
+++ Lab2/ex3.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sounddevice as sd
+from scipy.io.wavfile import write, read
+
+# a
+f = 100  # frecventa semnalului
+n_f = 10000  # numarul de esantioane
+t = n_f / f
+
+timp = np.linspace(0, t, n_f, endpoint=False)  # Vector de timp
+s = np.sin(2 * np.pi * f * timp)
+
+plt.plot(timp, s)
+plt.title('Punctul a)')
+plt.xlabel('Timp (s)')
+plt.ylabel('Amplitudine')
+plt.grid(True)
+# plt.show()
+
+
+# b
+frecventa = 500  # Frecventa semnalului în Hz
+durata = 3  # Durata semnalului în secunde
+frecventa_esantionare = 1500
+
+# Generarea timpului și a semnalului
+timp = np.arange(0, durata, 1 / frecventa_esantionare)  # Vector de timp
+semnal = np.sin(2 * np.pi * frecventa * timp)  # Semnalul sinusoidal
+
+# Afisarea grafică
+plt.plot(timp, semnal)
+plt.title('Semnal sinusoidal de 800 Hz')
+plt.xlabel('Timp (s)')
+plt.ylabel('Amplitudine')
+plt.grid(True)
+# plt.show()
+
+# c
+
+fr = 1000
+d = 1
+fr_esan = 1000
+
+t = np.arange(0, d, 1 / fr_esan)
+semnal_s = np.mod(t, 1 / fr) * fr
+
+plt.plot(t, semnal_s)
+plt.title('Semnal sawtooth de 240 Hz')
+plt.xlabel('Timp (s)')
+plt.ylabel('Amplitudine')
+plt.grid(True)
+# plt.show()
+
+
+# d
+frecv = 2000
+durata_s = 1
+frecv_es = 40000
+
+timp_square = np.arange(0, durata_s, 1 / frecv_es)
+semnal_square = np.sign(np.sin(2 * np.pi * frecv_es * timp_square))
+
+plt.plot(timp_square, semnal_square, color='red')
+plt.title('Semnal square de 300 Hz')
+plt.xlabel('Timp (s)')
+plt.ylabel('Amplitudine')
+plt.grid(True)
+# plt.show()
+
+
+# semnalul s
+sd.play(s, samplerate=n_f)
+sd.wait()  # Așteaptă până la sfârșitul redării
+
+# semnalul sinusoidal
+sd.play(semnal, samplerate=frecventa_esantionare)
+sd.wait()
+
+# semnalul sawtooth
+sd.play(semnal_s, samplerate=fr_esan)
+sd.wait()
+
+# semnalul square
+sd.play(semnal_square, samplerate=frecv_es)
+sd.wait()
+
+filename = "semnal_sinusoidal.wav"
+write(filename, n_f, (semnal_s * 32767).astype(np.int16))
+
+# Citirea fișierului salvat și verificarea
+rate, data = read(filename)
+plt.figure()
+plt.plot(data)
+plt.title('Semnalul citit din fișierul .wav')
+plt.xlabel('Eșantioane')
+plt.ylabel('Amplitudine')
+plt.grid(True)
+plt.show()
