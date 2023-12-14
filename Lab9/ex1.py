1ae3a9d9d085ec42f8459a33d6f7e924b453a863
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2023-12-14 16:27:27 +0200

    lab9

---
+++ Lab9/ex1.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+# Definirea componentelor
+N = 1000
+t = np.arange(N)
+trend = 0.02 * t**2
+seasonality = 10 * np.sin(2 * np.pi * t / 50) + 5 * np.cos(2 * np.pi * t / 30)
+noise = np.random.normal(0, 2, N)
+
+# Generarea seriei de timp
+time_series = trend + seasonality + noise
+
+# Desenarea seriei de timp și componentelor separat
+plt.figure(figsize=(12, 6))
+plt.subplot(4, 1, 1)
+plt.plot(t, time_series, label='Seria de timp generată')
+plt.legend()
+plt.title('Seria de timp generată')
+
+plt.subplot(4, 1, 2)
+plt.plot(t, trend, label='Trend')
+plt.legend()
+plt.title('Trend')
+
+plt.subplot(4, 1, 3)
+plt.plot(t, seasonality, label='Sezon')
+plt.legend()
+plt.title('Sezon')
+
+plt.subplot(4, 1, 4)
+plt.plot(t, noise, label='Zgomot')
+plt.legend()
+plt.title('Zgomot')
+
+plt.tight_layout()
+plt.show()
+
